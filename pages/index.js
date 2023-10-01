a9b68b13ef36db897af5d5f3d075e591effdb816
Agneya T <agneyat2@gmail.com>
2023-10-01 18:07:55 -0400

    make eslint happy

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import Head from 'next/head';
+import BasicGridImages from '@/components/gridGallery/BasicGridImages';
 import Associations from '../components/associations/Associations';
-import GridGallery from '../components/gridGallery/GridGallery';
 import HomeAbout from '../components/homeAbout/HomeAbout';
 import Screen from '../components/screen/Screen';
 import Sponsors from '../components/sponsors/Sponsors';
 import TechStack from '../components/techStack/TechStack';
 import Video from '../components/video/Video';
-import BasicGridImages from '@/components/gridGallery/BasicGridImages';
 
 export default function Home() {
   return (
